5cdbcc2a7c0442b2dcecb7d6d29cfd221cc80fe9
Julian Chodorowski <s15468@pjwstk.edu.pl>
2021-06-16 00:33:56 +0200

    Init project

---
+++ src/main/java/pl/pjatk/JulCho/JulChoApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.JulCho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulChoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulChoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/JulCho/JulChoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.JulCho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulChoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
